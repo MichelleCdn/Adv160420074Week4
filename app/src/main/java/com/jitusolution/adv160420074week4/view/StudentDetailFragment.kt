45cc3a3f9925f1f49d468f416f21df94cf6ed8f3
MichelleCdn <michellecdn.tab2@gmail.com>
2023-06-14 14:19:09 +0700

    week11

--- app/src/main/java/com/jitusolution/adv160420074week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/jitusolution/adv160420074week4/view/StudentDetailFragment.kt
@@ -2,6 +2,7 @@ package com.jitusolution.adv160420074week4.view
 
 import android.database.Observable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,10 +13,13 @@ import com.jitusolution.adv160420074week4.R
 import com.jitusolution.adv160420074week4.model.Student
 import com.jitusolution.adv160420074week4.util.loadImage
 import com.jitusolution.adv160420074week4.viewmodel.DetailViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_student_detail.*
 import kotlinx.android.synthetic.main.student_list_item.*
 import kotlinx.android.synthetic.main.student_list_item.txtId
 import kotlinx.android.synthetic.main.student_list_item.txtName
+import java.util.concurrent.TimeUnit
 
 
 class StudentDetailFragment : Fragment() {
@@ -64,14 +68,29 @@ class StudentDetailFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.studentLiveData.observe(viewLifecycleOwner){
 
-            val student = viewModel.studentLiveData.value
+            //val student = viewModel.studentLiveData.value
+            val student = it
+
             student?.let{
-                txtId.setText(student.id)
-                txtName.setText(student.name)
-                txtBod.setText(student.bod)
-                txtPhone.setText(student.phone)
+                txtId.setText(it.id)
+                txtName.setText(it.name)
+                txtBod.setText(it.bod)
+                txtPhone.setText(it.phone)
 
                 imageView2.loadImage(student.photoUrl, progressBar2)
+
+                btnNotif.setOnClickListener {
+                    io.reactivex.rxjava3.core.Observable.timer(5, TimeUnit.SECONDS)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe {
+                            Log.d("Messages", "five seconds")
+                            MainActivity.showNotification(student.name.toString(),
+                                "A new notification created",
+                                R.drawable.ic_baseline_person_24)
+                        }
+                }
+
             }
 
 
--- app/src/main/java/com/jitusolution/adv160420074week4/view/StudentListAdapter.kt
+++ app/src/main/java/com/jitusolution/adv160420074week4/view/StudentListAdapter.kt
@@ -7,52 +7,58 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.jitusolution.adv160420074week4.R
+import com.jitusolution.adv160420074week4.databinding.StudentListItemBinding
 import com.jitusolution.adv160420074week4.model.Student
 import com.jitusolution.adv160420074week4.util.loadImage
 import kotlinx.android.synthetic.main.fragment_student_detail.view.*
 import kotlinx.android.synthetic.main.student_list_item.view.*
 
 
-class StudentListAdapter(val studentList:ArrayList<Student>) :RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>()
+class StudentListAdapter(val studentList:ArrayList<Student>) :RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(), ButtonDetailClickListener
 {
-    class StudentViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class StudentViewHolder(var view: StudentListItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+//        val view = inflater.inflate(R.layout.student_list_item, parent, false)
+        val view = DataBindingUtil.inflate<StudentListItemBinding>(inflater,R.layout.student_list_item,parent,false)
         return StudentViewHolder(view)
 
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        val txtId = holder.view.findViewById<TextView>(R.id.txtId)
-//        txtId.text = studentList[position].id
+        holder.view.student=studentList[position]
+        holder.view.listener=this
+
+//        val txtId = holder.view.findViewById<TextView>(R.id.txtId)
+////        txtId.text = studentList[position].id
+////
+//        val txtName = holder.view.findViewById<TextView>(R.id.txtName)
+////        txtName.text = studentList[position].name
 //
-        val txtName = holder.view.findViewById<TextView>(R.id.txtName)
-//        txtName.text = studentList[position].name
+//        val student = studentList[position]
 
-        val student = studentList[position]
-
-        with(holder.view){
-            txtId.text=student.id
-            txtName.text=student.name
-            btnDetail.setOnClickListener {
-//            val action = StudentListFragmentDirections.actionStudentDetail()
-//            Navigation.findNavController(it).navigate(action)
-                val action = student.id?.let{ id ->
-                    StudentListFragmentDirections.actionStudentDetail(id)
-                }
-                if (action != null)
-                {
-                    Navigation.findNavController(it).navigate(action)
-                }
-
-            }
-            imageView.loadImage(student.photoUrl, progressBar)
-        }
+//        with(holder.view){
+//            txtId.text=student.id
+//            txtName.text=student.name
+//            btnDetail.setOnClickListener {
+////            val action = StudentListFragmentDirections.actionStudentDetail()
+////            Navigation.findNavController(it).navigate(action)
+//                val action = student.id?.let{ id ->
+//                    StudentListFragmentDirections.actionStudentDetail(id)
+//                }
+//                if (action != null)
+//                {
+//                    Navigation.findNavController(it).navigate(action)
+//                }
+//
+//            }
+//            imageView.loadImage(student.photoUrl, progressBar)
+//        }
 
     }
 
@@ -66,5 +72,10 @@ class StudentListAdapter(val studentList:ArrayList<Student>) :RecyclerView.Adapt
         studentList.clear()
         studentList.addAll(newStudentList)
         notifyDataSetChanged()
+    }
+
+    override fun onButtonDetailClick(v: View) {
+        val action = StudentListFragmentDirections.actionStudentDetail(v.tag.toString())
+        Navigation.findNavController(v).navigate(action)
     }
 }
